23bfd0920824024c9dcad97f1fd51cb666de46ac
engstk <eng.stk@sapo.pt>
2014-12-15 13:40:54 +0000

    blu_active: dynamic cpufreq policy governor designed for latency-sens…
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -519,6 +519,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
 extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_smartmax_eps)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 /*********************************************************************
