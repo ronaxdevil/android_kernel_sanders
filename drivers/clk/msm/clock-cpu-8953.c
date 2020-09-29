c8d2c14255a1b82eb93a21f38f4b310804625a59
ronaxdevil <pratabidya.007@gmail.com>
2020-09-29 09:35:01 +0530

    qcom-cpufreq: Remove CLKFLAG_NO_RATE_CACHE
    Revert "qcom-cpufreq: Use CLKFLAG_NO_RATE_CACHE"
    
    This reverts commit c9a08a987a828c8108d73ec7d5b0985db65ffe0b.
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>
    
    Revert "clk: msm: clock-cpu-8996: Use CLKFLAG_NO_RATE_CACHE for perfcl_hf_mux"
    
    This reverts commit 3a7619fdc55e373c7e159c0ecfb7454957342927.
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>
    
    Revert "clk: msm: clock-cpu-8953: Use CLKFLAG_NO_RATE_CACHE for a53ssmux_perf/pwr"
    
    This reverts commit 8e0f538ed85d833294d150f8b80813b930ef4c5c.
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>
    
    Revert "clk: msm: clock-cpu-8939: Use CLKFLAG_NO_RATE_CACHE for a53ssmux_bc/lc"
    
    This reverts commit 905b071723ccd09118931319201dda146c7836ca.
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- drivers/clk/msm/clock-cpu-8953.c
+++ drivers/clk/msm/clock-cpu-8953.c
@@ -156,7 +156,6 @@ static struct mux_div_clk a53ssmux_perf = {
 	},
 	.c = {
 		.dbg_name = "a53ssmux_perf",
-		.flags = CLKFLAG_NO_RATE_CACHE,
 		.ops = &clk_ops_mux_div_clk,
 		CLK_INIT(a53ssmux_perf.c),
 	},
@@ -178,7 +177,6 @@ static struct mux_div_clk a53ssmux_pwr = {
 	},
 	.c = {
 		.dbg_name = "a53ssmux_pwr",
-		.flags = CLKFLAG_NO_RATE_CACHE,
 		.ops = &clk_ops_mux_div_clk,
 		CLK_INIT(a53ssmux_pwr.c),
 	},
